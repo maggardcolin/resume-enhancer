c689d77e2a129decbc1019d7e202833555ad6b18
maggardcolin@gmail.com <110071999+maggardcolin@users.noreply.github.com>
2024-02-24 11:27:56 -0600

    added parser.py

---
+++ parser.py
@@ -0,0 +1,3 @@
+#input - json file with job description information
+#processing - count each occurrence of word, not case sensitive (not including field names)
+#output json file with each word getting its own field and having a value equal to its frequency
