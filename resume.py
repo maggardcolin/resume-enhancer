3bce0dc8d5aafd3c2698d4044d8154796d0f7146
Gorytitan <npastore2204@gmail.com>
2024-02-24 12:16:48 -0600

    reupload resume.py

--- resume.py
+++ resume.py
@@ -1,3 +1,13 @@
 activities = []
 educations = []
 projects = []
+
+def addActivity(organization, location, role_title, description):
+    print("bleh")
+def addExperience(company, role_title, location, duration, description):
+    print("bleh")
+def addProject(name, languages_used, description):
+    print("bleh")
+
+def compileResume():
+    print("bleh")
