14897e1c5d1d169a232c15dbdd8f73d5fec1ff9e
Gorytitan <npastore2204@gmail.com>
2024-02-24 13:06:18 -0600

    temp Resume Objects added, Resume.py adding functions " finished\

--- resume.py
+++ resume.py
@@ -1,28 +1,32 @@
-from ResumeObjects import Activity
-from ResumeObjects import Experience
-from ResumeObjects import Projects
-activities = []
-experiences = []
-educations = []
-projects = []
-skills = ""
-def addEducation(degree, date, location, gpa, description, delimiter):
-    pass
+from ResumeObjects import Activity, Education, Experience, Projects
+from docx import Document
 
-def addActivity(organization, location, role_title, description , delimiter):
-    a1 = Activity(organization, location, role_title, description, delimiter)
-    activities.append(a1)
+class Resume:
+    def __init__(self):
+        self.activities = []
+        self.experiences = []
+        self.educations = []
+        self.projects = []
+        self.skills = ""
 
-def addExperience(company, role_title, location, duration, description, delimiter):
-    e1 = Experience(company, role_title, location, duration, description, delimiter)
-    experiences.append(e1)
+    def addEducation(self, degree, date, location, gpa, description, delimiter):
+        pass
 
-def addProject(name, languages_used, description, delimiter):
-    p1 = Projects(name, languages_used, description, delimiter)
-    projects.append(p1)
+    def addActivity(self, organization, location, role_title, description , delimiter):
+        a1 = Activity(organization, location, role_title, description, delimiter)
+        self.activities.append(a1)
 
-def addSkills(skill):
-    skills += skill
-    
-def compileResume():
-    print("ble")
+    def addExperience(self, company, role_title, location, duration, description, delimiter):
+        e1 = Experience(company, role_title, location, duration, description, delimiter)
+        self.experiences.append(e1)
+
+    def addProject(self, name, languages_used, description, delimiter):
+        p1 = Projects(name, languages_used, description, delimiter)
+        self.projects.append(p1)
+
+    def addSkills(self, skill):
+        self.skills = self.skills + ", " + skill
+
+    def compileResume(self):
+        doc = Document()
+            
