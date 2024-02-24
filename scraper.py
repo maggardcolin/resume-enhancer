25cc52af32278f7fb569b5fc027cd1d572993f56
maggardcolin@gmail.com <110071999+maggardcolin@users.noreply.github.com>
2024-02-24 12:05:00 -0600

    added colin branch

--- scraper.py
+++ scraper.py
@@ -1,1 +1,9 @@
 #colin
+import re, json
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
