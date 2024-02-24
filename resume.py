275b8c7bc3f59bd23e48ebb6eb563c9326ba507d
Gorytitan <npastore2204@gmail.com>
2024-02-24 12:54:42 -0600

    added addEducation

--- resume.py
+++ resume.py
@@ -1,13 +1,28 @@
+from ResumeObjects import Activity
+from ResumeObjects import Experience
+from ResumeObjects import Projects
 activities = []
+experiences = []
 educations = []
 projects = []
+skills = ""
+def addEducation(degree, date, location, gpa, description, delimiter):
+    pass
 
 def addActivity(organization, location, role_title, description , delimiter):
-    print("bleh")
-def addExperience(company, role_title, location, duration, description, delimiter):
-    print("bleh")
-def addProject(name, languages_used, description, delimiter):
-    print("bleh")
+    a1 = Activity(organization, location, role_title, description, delimiter)
+    activities.append(a1)
 
+def addExperience(company, role_title, location, duration, description, delimiter):
+    e1 = Experience(company, role_title, location, duration, description, delimiter)
+    experiences.append(e1)
+
+def addProject(name, languages_used, description, delimiter):
+    p1 = Projects(name, languages_used, description, delimiter)
+    projects.append(p1)
+
+def addSkills(skill):
+    skills += skill
+    
 def compileResume():
     print("ble")
