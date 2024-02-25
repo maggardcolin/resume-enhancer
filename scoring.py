b6ec9a23d0d1fe68a604b8aec3f7430652c73ab3
maggardcolin@gmail.com <110071999+maggardcolin@users.noreply.github.com>
2024-02-24 21:49:47 -0600

    final messages

--- scoring.py
+++ scoring.py
@@ -5,44 +5,77 @@ def scoreResume(file_path, my_resume):
     key_count = 0
     json_data = file_path
     df = pd.read_json(json_data)
-    my_list = df.values.tolist()
-    for i in range(len(my_list)):
-        keyword = my_list[i][0]
+    word_list = df.values.tolist()
+    printStatements = False
+    matched_words = []
+
+    for i in range(len(word_list)):
+        keyword = word_list[i][0]
         activities = my_resume.activities
         experiences = my_resume.experiences
         projects = my_resume.projects
-        
+
         if keyword in my_resume.objective:
-            print(keyword + " matched!")
+            if printStatements:
+                print(keyword + " matched!")
             key_count += 1
+            matched_words.append(keyword)
         if keyword in my_resume.skills:
-            print(keyword + " matched!")
+            if printStatements:
+                print(keyword + " matched!")
             key_count += 1
+            matched_words.append(keyword)
         for activity in activities:
             if keyword in activity.get_description():
-                print(keyword + " matched!")
+                if printStatements:
+                    print(keyword + " matched!")
                 key_count += 1
+                matched_words.append(keyword)
             if keyword in activity.get_role():
-                print(keyword + " matched!")
+                if printStatements:
+                    print(keyword + " matched!")
                 key_count += 1
+                matched_words.append(keyword)
             if keyword in activity.get_organization():
-                print(keyword + " matched!")
+                if printStatements:
+                    print(keyword + " matched!")
                 key_count += 1
+                matched_words.append(keyword)
         for experience in experiences:
             if keyword in experience.get_description():
-                print(keyword + " matched!")
+                if printStatements:
+                    print(keyword + " matched!")
                 key_count += 1
+                matched_words.append(keyword)
             if keyword in experience.get_role():
-                print(keyword + " matched!")
+                if printStatements:
+                    print(keyword + " matched!")
                 key_count += 1
+                matched_words.append(keyword)
             if keyword in experience.get_company():
-                print(keyword + " matched!")
+                if printStatements:
+                    print(keyword + " matched!")
                 key_count += 1
+                matched_words.append(keyword)
         for project in projects:
             if keyword in project.description:
-                print(keyword + " matched!")
+                if printStatements:
+                    print(keyword + " matched!")
                 key_count += 1
+                matched_words.append(keyword)
             if keyword in activity.languages:
-                print(keyword + " matched!")
+                if printStatements:
+                    print(keyword + " matched!")
                 key_count += 1
-    print(key_count)    
+                matched_words.append(keyword)
+    print("\n" + str(key_count) + " matches found. Let's shoot for more!")
+    print("Here are the top 10 matched words for job descriptions matching your query: ")
+    printed_keywords = 0  # Counter for printed keywords
+    for i in range(len(word_list)):
+        if printed_keywords >= 10:
+            break
+        keyword = word_list[i][0]
+        if keyword not in matched_words:
+            match_count = word_list[i][1]
+            print(str(printed_keywords + 1) + ") \"" + keyword + f"\" with {match_count} matches")
+            printed_keywords += 1
