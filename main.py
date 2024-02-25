862d894426881e7a87c7c8e91cbc7b0806810aef
maggardcolin@gmail.com <110071999+maggardcolin@users.noreply.github.com>
2024-02-24 20:02:00 -0600

    changed main

--- main.py
+++ main.py
@@ -96,9 +96,10 @@ def add_act():
 
 # add a project
 def add_proj():
+    project_name = input("Enter project name: ")
     langs = input("Enter any technologies/languages used (e.g 'Java, Python, C'): ")
     description = input("Please enter the description of this project using '@' to separate the bullet points: ")
-    my_resume.add_project(langs, description, delimiter = "@")
+    my_resume.add_project(name, langs, description, delimiter = "@")
 
 # add skills
 def add_skill():
